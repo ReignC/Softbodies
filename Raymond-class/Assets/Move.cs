4575f968939b632ffd5bd42cf26a8b6f5d82d0f3
Obelush <124471029+ReignC@users.noreply.github.com>
2023-04-29 14:23:45 -0700

    added movement

---
+++ Raymond-class/Assets/Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int myInt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myInt = myInt + 1;
+        Debug.Log (myInt);
+
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
